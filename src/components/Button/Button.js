ba78bafcd07f5b4422635a0158a15cd8e72e9595
Franklin Farahani <dduction@gmail.com>
2018-09-11 10:54:21 -0700

    change default button styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 const Button = styled.a`
   text-decoration: none;
   font-size: .9em;
-  background: black;
+  background-image: linear-gradient(to right, #DD5E89 0%, #F7BB97 100%);
   color: white;
   height: 37px;
   line-height: 37px;
   padding: 0 16px;
-  border: 1px solid rgba(0,0,0,.15);
   border-radius: 4px;
+  box-shadow: 0 0px 6px 4px hsla(340, 65%, 62%, 0.2);
   &:hover {
-    background: blue;
+    background-position: right center;
   }
 `
 
