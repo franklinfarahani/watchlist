b0e12545944cc8824c7551a5af4e575106c2c2f7
Franklin Farahani <dduction@gmail.com>
2018-09-15 13:12:52 -0700

    change Button to render button instead of react-router Link component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { colors } from '../../utils/GlobalStyles';
 
-const Button = styled(Link)`
+const Button = styled.button`
+  display: flex;
+  align-items: center;
   background-origin: border-box;
   text-decoration: none;
   font-size: .9em;
-  background-image: ${(props) => props.category === 'primary' ? 'linear-gradient(to bottom, ' + colors.SECONDARY + ' 0%,' + colors.PRIMARY+' 100%)' : colors.white};
+  background-image: ${(props) => props.category === 'primary' && 'linear-gradient(to bottom, ' + colors.SECONDARY + ' 0%,' + colors.PRIMARY+' 100%)'};
+  background-color: ${colors.WHITE};
   color: ${(props) => props.category === 'primary' ? 'white' : colors.PRIMARY};
   height: 37px;
-  line-height: 37px;
   padding: 0 16px;
   border: ${(props) => props.category === 'primary' ? 'solid 2px transparent' : 'solid 2px ' + colors.PRIMARY};
   border-radius: 4px;
