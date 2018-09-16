22d2896e7a5e039bae299e76248f3df6519c1ac8
Franklin Farahani <dduction@gmail.com>
2018-09-16 10:05:30 -0700

    refactor and expand Button Features

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,22 +1,57 @@
 import styled from 'styled-components';
 import { colors } from '../../utils/GlobalStyles';
 
+const bgImage = ({ category }) => {
+  return category ==='primary' && 'linear-gradient(to bottom, ' + colors.SECONDARY + ' 0%,' + colors.PRIMARY+' 50%,' + colors.SECONDARY + ' 100%)';
+}
+
+const textColor = ({ category }) => {
+  return category ==='primary' ? 'white' : colors.PRIMARY;
+}
+
+const borderColor = ({ category }) => {
+  return category ==='primary' ? 'solid 2px transparent' : 'solid 2px ' + colors.PRIMARY;
+}
+
+const hoverBgPosition = ({ category }) => {
+  return category === 'primary' ? 'bottom' : 'top';
+}
+
+const hoverBgImage = ({ category }) => {
+  return category !== 'primary' && 
+    'linear-gradient(to bottom, ' + colors.SECONDARY + ' 0%,' + colors.PRIMARY+' 50%,' + colors.SECONDARY + ' 100%)';
+}
+
+const hoverBorderColor = ({ category }) => {
+  return category !=='primary' && 'solid 2px transparent';
+}
+
+const hoverTextColor = ({ category }) => {
+  return category !=='primary' && colors.WHITE;
+}
+
 const Button = styled.button`
   display: flex;
   align-items: center;
   background-origin: border-box;
   text-decoration: none;
   font-size: .9em;
-  background-image: ${(props) => props.category === 'primary' && 'linear-gradient(to bottom, ' + colors.SECONDARY + ' 0%,' + colors.PRIMARY+' 100%)'};
+  background-image: ${bgImage};
+  background-size: auto 200%;
   background-color: ${colors.WHITE};
-  color: ${(props) => props.category === 'primary' ? 'white' : colors.PRIMARY};
+  color: ${textColor};
   height: 37px;
   padding: 0 16px;
-  border: ${(props) => props.category === 'primary' ? 'solid 2px transparent' : 'solid 2px ' + colors.PRIMARY};
+  border: ${borderColor};
   border-radius: 4px;
+  background-position: top;
+  transition: background .3s;
   
   &:hover {
-    background-position: right center;
+    background-position: ${hoverBgPosition};
+    background-image: ${hoverBgImage};
+    border: ${hoverBorderColor};
+    color: ${hoverTextColor}
   }
 `
 
