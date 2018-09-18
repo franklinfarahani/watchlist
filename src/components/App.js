bec2c05d7da79fa8624dda1bb81cb27fa54671cf
Franklin Farahani <dduction@gmail.com>
2018-09-17 18:17:49 -0700

    restructure and overhaul main app container and PrivateRoute logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled, { createGlobalStyle } from 'styled-components';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlay, faSearch, faTimes, faImage } from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { fetchUser } from '../actions';
-import { connect } from 'react-redux';
-
-import Nav from './Nav';
-import Search from './Search';
-import requireAuth from './auth/requireAuth';
-
-import SignIn from '../pages/SignIn/SignIn';
-import Watchlist from '../pages/Watchlist/Watchlist';
-import Movies from '../pages/Movies/Movies';
-import TV from '../pages/TV/TV';
 import AppWrapper from '../pages/AppWrapper'
 
 
-// Fontawesome custom icon library
-library.add(faPlay, faSearch, faTimes, faImage);
 
-// Styles
-
-const ResetGlobalStyle = createGlobalStyle`
-  padding: 0;
-  margin: 0;
-  
-  input:focus,
-  select:focus,
-  textarea:focus,
-  button:focus {
-    outline: none;
-  }
-
-  input[type="search"]::-webkit-search-cancel-button {
-  -webkit-appearance: none;
-  }
-
-  button {
-    background: transparent;
-    border : none;
-    font: inherit;
-    cursor: pointer;
-  }
-`
-
-const AppWrapperRENAME = styled.div`
-  max-width: 720px;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-`
-
-const AppFooter = styled.div`
-  margin-top: 50px;
-  padding: 20px;
-  text-align: center;
-`
-const AppTitle = styled.div`
-  font-size: .8em;
-`
 
 class App extends Component {
-
-  
 
   render() {
     return (
       <Router>
-        <AppWrapper>
-          
-          <Nav />
-          <Search />
-          <Switch>
-            <Route exact path='/' component={SignIn} />
-            <Route path='/app' component={requireAuth(Watchlist)} />
-            <Route path='/movies' component={Movies} />
-            <Route path='/tv' component={TV} />
-          </Switch>
-          <AppFooter>
-            <AppTitle>&copy; Copyright Franklin Farahani 2018</AppTitle>
-          </AppFooter>
-          <ResetGlobalStyle />
-        </AppWrapper>
+        <AppWrapper />
       </Router>
     );
   }
--- src/pages/AppWrapper.js
+++ src/pages/AppWrapper.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import styled from 'styled-components';
-import { fetchUser } from '../actions';
+import { withRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { verifyAuth } from '../actions';
+
+import styled, { createGlobalStyle } from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlay, faSearch, faTimes, faImage } from '@fortawesome/free-solid-svg-icons'
+
+import Nav from '../components/Nav';
+import Search from '../components/Search';
+// import requireAuth from './auth/requireAuth';
+
+import SignIn from '../pages/SignIn/SignIn';
+import Watchlist from '../pages/Watchlist/Watchlist';
+import Movies from '../pages/Movies/Movies';
+import TV from '../pages/TV/TV';
+
+// Fontawesome custom icon library
+library.add(faPlay, faSearch, faTimes, faImage);
+
+// Styles
+
+const ResetGlobalStyle = createGlobalStyle`
+  padding: 0;
+  margin: 0;
+  
+  input:focus,
+  select:focus,
+  textarea:focus,
+  button:focus {
+    outline: none;
+  }
+
+  input[type="search"]::-webkit-search-cancel-button {
+  -webkit-appearance: none;
+  }
+
+  button {
+    background: transparent;
+    border : none;
+    font: inherit;
+    cursor: pointer;
+  }
+`
+
+const AppFooter = styled.div`
+  margin-top: 50px;
+  padding: 20px;
+  text-align: center;
+`
+const AppTitle = styled.div`
+  font-size: .8em;
+`
 
 const Wrapper = styled.div`
   max-width: 720px;
@@ -11,20 +60,57 @@ const Wrapper = styled.div`
   flex-direction: column;
 `
 
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/SignIn', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/app' />}
+    />
+  )
+}
 
 class AppWrapper extends Component{
   
-  componentDidMount() {
-    this.props.fetchUser();
-  }
   render(){
     return (
       // styled-components uses Pure Components which means location needs to be explicitly passed
       <Wrapper location={this.props.location}>
-        {this.props.children}
+        <Nav />
+          <Search />
+          <Switch>
+            <PublicRoute authed={this.props.authenticated} exact path='/' component={Movies} />
+            <PublicRoute authed={this.props.authenticated} path='/SignIn' component={SignIn} />
+            <PrivateRoute authed={this.props.authenticated} path='/app' component={Watchlist} />
+            <Route path='/movies' component={Movies} />
+            <Route path='/tv' component={TV} />
+            <Route render={() => <h3>No Match</h3>} />
+          </Switch>
+          <AppFooter>
+            <AppTitle>&copy; Copyright Franklin Farahani 2018</AppTitle>
+          </AppFooter>
+          <ResetGlobalStyle />
       </Wrapper>
     )
   }
 }
 
-export default withRouter(connect(null, { fetchUser }, null, { pure: false })(AppWrapper));
+function mapStateToProps({auth}) {
+  return { 
+    authenticated: auth.authenticated
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(AppWrapper));
