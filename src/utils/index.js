6007ee7b6060e9a7e21ed58472452e2b4bbf6ca8
Franklin Farahani <dduction@gmail.com>
2018-09-23 11:38:00 -0700

    create helper functions to convert genreIds to names and format date str

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
-export default function imagesLoaded(parentNode) {
+import genres from '../config/genreIds.json'
+
+export function imagesLoaded(parentNode) {
   const imgElements = [...parentNode.querySelectorAll("img")];
   for (let i = 0; i < imgElements.length; i += 1) {
     const img = imgElements[i];
@@ -7,4 +9,62 @@ export default function imagesLoaded(parentNode) {
     }
   }
   return true;
+}
+
+export function getGenreName(id, type){
+  return type === 'movie' ? 
+    genres.movies[genres.movies.findIndex(genre => genre.id === id)].name :
+    genres.tv[genres.tv.findIndex(genre => genre.id === id)].name;
+}
+
+export function formatDate (date){
+  
+  let month;
+
+  switch (date.split("-")[1]) {
+    case '01':
+      month = 'January'
+      break;
+    case '02':
+      month = 'February'
+      break;
+    case '03':
+      month = 'March'
+      break;
+    case '04':
+      month = 'April'
+      break;
+    case '05':
+      month = 'May'
+      break;
+    case '06':
+      month = 'June'
+      break;
+    case '07':
+      month = 'July'
+      break;
+    case '08':
+      month = 'August'
+      break;
+    case '09':
+      month = 'September'
+      break;
+    case '10':
+      month = 'October'
+      break;
+    case '11':
+      month = 'November'
+      break;
+    case '12':
+      month = 'December'
+      break;
+    default:
+    break;
+  }
+  
+  return {
+    year: date.split("-")[0],
+    month,
+    day: date.split("-")[2]
+  }
 }
