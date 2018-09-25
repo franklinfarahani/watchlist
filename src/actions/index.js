eab377dcfeaa2ea4602f31980f84d8da863213ee
Franklin Farahani <dduction@gmail.com>
2018-09-24 19:52:31 -0700

    add actions for getting imdb id and looking up ratings

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,51 @@
 import { listRef, authRef, provider } from '../firebase';
-import {tmdb} from '../config/keys';
-import { SEARCH_TITLES, FETCH_LIST, AUTH_USER, AUTH_ERROR, SIGN_OUT_USER } from '../actions/types';
+import {tmdb, omdb} from '../config/keys';
+import { SEARCH_TITLES, GET_IMDB_ID, GET_TITLE, FETCH_LIST, AUTH_USER, AUTH_ERROR, SIGN_OUT_USER } from '../actions/types';
 
 // let nextPosition = 0;
 
 export const searchTitles = (query) => dispatch => {
   fetch(`${tmdb.url}/search/multi?api_key=${tmdb.key}&query=${query}`)
-    .then(response => {
+    .then(response => response.json())
+    .then(result => {
       dispatch({
         type: SEARCH_TITLES,
+        payload: result
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: SEARCH_TITLES,
+        error
+      });
+    });
+}
+
+
+export function getImdbId(tmdbId){
+  return function(dispatch) {
+  fetch(`${tmdb.url}/movie/${tmdbId}/external_ids?api_key=${tmdb.key}`)
+    .then(response => response.json())
+    .then(result => {
+      dispatch({
+        type: GET_IMDB_ID,
+        payload: result.imdb_id
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_IMDB_ID,
+        error
+      });
+    });
+  }
+}
+
+export const getTitle = (imdbId) => dispatch => {
+  fetch(`${omdb.url}/?apikey=${omdb.key}&i=${imdbId}`)
+    .then(response => {
+      dispatch({
+        type: GET_TITLE,
         payload: response.json()
       });
     });
