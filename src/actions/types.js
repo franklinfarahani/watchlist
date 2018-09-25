c8bf322d455d252761a27bfb7a3ddd78a92743a8
Franklin Farahani <dduction@gmail.com>
2018-09-24 19:52:45 -0700

    add new action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,6 @@
 export const SEARCH_TITLES = 'SEARCH_TITLES';
+export const GET_IMDB_ID = 'GET_IMDB_ID';
+export const GET_TITLE = 'GET_TITLE';
 export const AUTH_USER = 'AUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
