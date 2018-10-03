560b21a6b8e7e12deff190c35bdc8ab764841a03
Franklin Farahani <dduction@gmail.com>
2018-10-03 16:17:36 -0700

    refactor media route and add new list route that returns user's list

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,7 +25,23 @@ const listRef = databaseRef.child('lists');
 
 const app = express();
 
-app.use(compression());
+interface Media {
+  id: number,
+  media_type: string,
+  title: string,
+  poster: string,
+  year: number,
+  release_date: string,
+  genres: Array<number>,
+  synopsis?: string,
+  ratings?: Array<object>,
+  runtime?: number,
+  viewing_options?: Array<object>
+}
+
+interface AuthRequest extends express.Request {
+  user: any 
+}
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +49,7 @@ app.use(compression());
 |--------------------------------------------------------------------------
 |
 | includes:
-|
+|   
 |   * Authentication: 
 |       For routes that use it, checks to see whether the request's
 |       user is authenticated.
@@ -54,12 +70,39 @@ const authenticate = async (req, res, next) => {
     req.user = decodedIdToken;
     next();
     return;
-  } catch(e) {
+  } 
+  catch(e) {
+    // 403: Forbidden
     res.status(403).send('Unauthorized');
     return;
   }
 };
 
+async function getTitle(id: number, type: "movie" | "show") {
+  try {
+    const res = await fetch(`https://apis.justwatch.com/content/titles/${type}/${id}/locale/en_CA`);
+    const json = await res.json();
+    const item: Media = {
+      id: json.id,
+      media_type: type,
+      title: json.title,
+      poster: json.poster.split("{")[0],
+      year: json.original_release_year,
+      release_date: json.localized_release_date,
+      genres: json.genre_ids,
+      synopsis: json.short_description,
+      ratings: json.scoring,
+      viewing_options: json.offers
+    };
+    type === 'movie' ? item.runtime = json.runtime : null;
+    return {item, error: null};
+  }
+  catch(err) {
+    return {item: null, error: err};
+  }
+}
+
+app.use(compression());
 app.use("/list", authenticate);
 
 /*
@@ -75,53 +118,86 @@ app.use("/list", authenticate);
 |
 */
 
-app.get("/media", (req, resp) => {
-  let mediaId = req.query.id;
-  let mediaType = req.query.type;
-  fetch(`https://apis.justwatch.com/content/titles/${mediaType}/${mediaId}/locale/en_CA`)
-    .then(res => res.json())
-    .then(json => {
-      interface Media {
-        id: number,
-        media_type: string,
-        title: string,
-        poster: string,
-        year: number,
-        release_date: string,
-        genres: Array<number>,
-        synopsis?: string,
-        ratings?: Array<object>,
-        runtime?: number,
-        viewing_options?: Array<object>
-      }
-      const item: Media = {
-        id: json.id,
-        media_type: mediaType,
-        title: json.title,
-        poster: json.poster.split("{")[0],
-        year: json.original_release_year,
-        release_date: json.localized_release_date,
-        genres: json.genre_ids,
-        synopsis: json.short_description,
-        ratings: json.scoring,
-        viewing_options: json.offers
-      };
-      mediaType === 'movie' ? item.runtime = json.runtime : null;
-      resp.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-      resp.status(200).send(item);
-    })
-    .catch(err => {
-      console.error(err);
-      resp.send(202);
-    });
+app.get("/search", async (req, resp) => {
+  let query = req.query.query;
+  try {
+    const res = await fetch(`https://apis.justwatch.com/content/titles/en_CA/popular?body={"content_types":["show","movie"],"page":1,"page_size":8,"query":"${query}"}`);
+    const json = await res.json();
+    
+    const searchResults = {
+      page: json.page,
+      page_size: json.page_size,
+      total_pages: json.total_pages,
+      total_results: json.total_results,
+      items: []
+    }
+    json.items.map(item => searchResults.items.push(
+      {
+        id: item.id,
+        media_type: item.object_type,
+        title: item.title,
+        poster: item.poster.split("{")[0],
+        year: item.original_release_year,
+        release_date: item.localized_release_date,
+        genres: item.genre_ids,
+        synopsis: item.short_description,
+        ratings: item.scoring
+      })
+    )
+    
+    resp.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    resp.status(200).send(searchResults);
+  }
+  catch(err) {
+    console.error(err);
+    // 400: Bad Request
+    resp.sendStatus(400);  
+  }
 });
 
-interface IGetUserAuthInfoRequest extends express.Request {
-  user: any 
-}
+app.get("/media/:type/:id", async (req, resp) => {
+  let mediaId = req.params.id;
+  let mediaType = req.params.type;
+  const result = await getTitle(mediaId, mediaType);
+  if (!result.error){
+    resp.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    resp.status(200).send(result.item);
+  } else {
+    console.error(result.error);
+    // 400: Bad Request
+    resp.sendStatus(400);  
+  }   
+});
 
-app.get("/list", (req: IGetUserAuthInfoRequest, resp) => {
-  listRef.child(req.user.uid).once("value", snapshot => resp.status(200).send(snapshot.val()));
+app.get("/list", async (req: AuthRequest, resp) => {
+  let listObj = {};
+  let watchlist = [];
+  try {
+    // Fetch given user's list from firebase database and assign it to listObj for operations
+    await listRef.child(req.user.id)
+    // await listRef.child('testuser') //Uncomment to test without user authentication
+      .once("value", snapshot => listObj = snapshot.val());
+    
+    // convert json object of media objects to array of objects
+    const list = Object.keys(listObj).map(index => listObj[index])
+    
+    // Make sure each getTitle promise is resolved and add them to new watchlist array that gets sent back
+    await Promise.all(
+      list.map(async item => {
+        const currentItem = await getTitle(item.id, item.media_type);
+        watchlist.push(currentItem.item);
+      })
+    )
+
+    resp.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    resp.status(200).send(watchlist);
+      
+  }
+  catch(err) {
+    console.error(err);
+    // 500: internal server error
+    resp.sendStatus(500);
+  }
 });
 
 export const api = functions.https.onRequest(app);
