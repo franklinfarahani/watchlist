7a559c94dd5a2e8121c947f0d8828b7e552fce24
Franklin Farahani <dduction@gmail.com>
2018-10-07 15:22:16 -0700

    create fresh and rotten icon components for RottenTomatoes

---
+++ src/components/BrandIcon/RtFreshIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import rtFreshIcon from '../../assets/icons/rtFresh.svg';
+
+const RtFreshIcon = ({size}) => {
+  return(
+    <img src={rtFreshIcon} height={size} alt="rotten tomatoes fresh" />
+  )
+}
+
+export default RtFreshIcon;
---
+++ src/components/BrandIcon/RtRottenIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import rtRottenIcon from '../../assets/icons/rtRotten.svg';
+
+const RtRottenIcon = ({size}) => {
+  return(
+    <img src={rtRottenIcon} height={size} alt="rotten tomatoes rotten" />
+  )
+}
+
+export default RtRottenIcon;
