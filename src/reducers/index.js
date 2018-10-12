f1d584089bd9afa770de1c86e6cb29eec31f2b5b
Franklin Farahani <dduction@gmail.com>
2018-10-12 10:30:18 -0700

    remove deprecated ratings reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 import search from './SearchReducer';
 import watchlist from './ListReducer';
-import imdb from './RatingsReducer';
 import auth from './AuthReducer';
 
 export default combineReducers({
   search,
   watchlist,
-  imdb,
   auth
 })
