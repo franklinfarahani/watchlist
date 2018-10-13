34069418185d395026d0f30535bc0ce5b57f42d0
Franklin Farahani <dduction@gmail.com>
2018-10-12 18:13:00 -0700

    style the Under Construction message

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { colors, shadows } from '../../config/styleVariables';
+import { colors } from '../../config/styleVariables';
+import {Warning as mdWarning} from 'styled-icons/material/Warning';
+
+const IconWarning = styled(mdWarning)`
+  color: ${colors.subtitle.MEDIUM};
+  width: 80px;
+`
 
 const DiscoverContainer = styled.div`
-  /* TODO: get rid of space between search bar and watchlist */
-  background: ${colors.WHITE};
   display: flex;
-  border-radius: 4px;
-  box-shadow: ${shadows.VERYLOW};
-  margin: 50px 0 0;
-  padding: 30px;
+  flex-direction: column;
+  flex: 1;
+  margin: 0;
+  width: 100%;
+`
+
+const EmptyList = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  margin: 0 auto;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h4 {    
+      font-size: 18px;
+      font-weight: 700;
+      line-height: 36px;
+      color: ${colors.subtitle.MEDIUM}
+    }
+
+    p {
+      font-size: 14px;
+      line-height: 22px;
+      color: ${colors.subtitle.MEDIUM}
+      width: 200px;
+      text-align: center;
+    }
+  }
 `
 
 class Discover extends Component {
   render() {
     return (
       <DiscoverContainer>
-        {`Discover New ${this.props.type}s!`}
+        <EmptyList>
+          <div>
+            <IconWarning title='Warning Icon' />
+            <h4>UNDER CONSTRUCTION</h4>
+            <p>{`Soon you'll be able to discover new ${this.props.type.toLowerCase()}s right here!`}</p>
+          </div>              
+        </EmptyList>
       </DiscoverContainer>
     )
   }
