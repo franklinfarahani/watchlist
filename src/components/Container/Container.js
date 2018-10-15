a77b300e97ec9457f63997e52440fd325a74e6d6
Franklin Farahani <dduction@gmail.com>
2018-10-15 12:07:51 -0700

    create Container component

---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin: 0;
+  width: 100%;
+`
+
+export default Container;
