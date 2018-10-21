8a7aedb260b476025d74765d1e10c424f652e596
Franklin Farahani <dduction@gmail.com>
2018-10-20 17:02:35 -0700

    pass location to discover from parent

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Discover from '../../components/Discover'
 
-const Movies = () => {
+const Movies = ({location}) => {
   return (
-    <Discover mediaType='movie' />
+    <Discover mediaType='movie' location={location}/>
   )
 }
+
+Movies.propTypes = {
+  location: PropTypes.object.isRequired
+};
 
 export default Movies;
--- src/pages/TV/TV.js
+++ src/pages/TV/TV.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Discover from '../../components/Discover'
 
-const TV = () => {
+const TV = ({location}) => {
   return (
-    <Discover mediaType='show' />
+    <Discover mediaType='show' location={location}/>
   )
 }
+
+TV.propTypes = {
+  location: PropTypes.object.isRequired
+};
 
 export default TV;
