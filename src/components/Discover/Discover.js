c2c8cea7388816170042db3957c0834ce8bb0dae
Franklin Farahani <dduction@gmail.com>
2018-10-23 13:11:00 -0700

    add styles to pagination

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -3,17 +3,33 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import queryString from 'query-string';
 import Paginate from 'react-paginate'; 
-
-import styled from 'styled-components';
-import { select } from '../../config/styleVariables';
 import DiscoverResults from './DiscoverResults';
 import Container from '../../components/Container';
 import GenreSelect from '../../components/GenreSelect'
 import genres from '../../config/genreIds.json';
 
-// TODO: fix default values for react-select
+import styled from 'styled-components';
+import { colors, select } from '../../config/styleVariables';
+import { ChevronLeft as faChevronLeft } from 'styled-icons/fa-solid/ChevronLeft';
+import { ChevronRight as faChevronRight } from 'styled-icons/fa-solid/ChevronRight';
+
+// TODO: update query format to one that does not require encoding. Use one of - _ . ~
 // TODO: build filtering system using react-select
 // TODO: build dropdown for sorting by Release date, Aggregate Rating
+
+const IconLeft = styled(faChevronLeft)`
+  color: ${colors.PRIMARY};
+  width: 12px;
+  margin-right: 24px;
+  margin-bottom: -1px;
+`
+
+const IconRight = styled(faChevronRight)`
+  color: ${colors.PRIMARY};
+  width: 12px;
+  margin-left: 24px;
+  margin-bottom: -1px;
+`
 
 const Controls = styled.div`
   display: flex;
@@ -30,6 +46,54 @@ const Controls = styled.div`
   & > div {
     width: 100%;
     flex-direction: row;
+  }
+`
+
+const PaginateWrapper = styled.div`
+  align-self: center;
+  margin-top: 34px;
+  .pagination {
+    display: flex;
+    align-items: center;
+    li {
+      position: relative;
+      padding: 14px 10px;
+      margin: 0 6px;
+      cursor: pointer;
+      color: ${colors.subtitle.MEDIUM};
+
+      a:focus {
+        outline: none;
+      }
+      
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 4px;
+        background: transparent;
+      }
+
+      &.disabled {
+        display: none;
+      }
+
+      &.break {
+        cursor: default;
+        color: ${colors.subtitle.LIGHT};
+      }
+      
+      &.active {
+        color: ${colors.BLACK};
+        cursor: default;
+        &::after {
+          background: linear-gradient(to right, ${colors.SECONDARY} 0%, ${colors.PRIMARY} 100%);
+        }
+      }
+    }
   }
 `
 
@@ -87,7 +151,7 @@ class Discover extends Component {
         query.page = page;
       }
       // query needs to be properly formatted and stringified from object to string
-      const stringifiedQuery = queryString.stringify(query);
+      const stringifiedQuery = queryString.stringify(query, {encode: false});
       history.push({
         search: stringifiedQuery,
       })
@@ -139,19 +203,21 @@ class Discover extends Component {
           page={page}
           pageSize={pageSize}
         />
-        <Paginate
-          previousLabel={<div>prev</div>}
-          nextLabel={"next"}
-          breakLabel={<a href="">...</a>}
-          breakClassName={"break-me"}
-          pageCount={totalPages}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={4}
-          onPageChange={this.handlePageChange}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}
-        />
+        <PaginateWrapper>
+          <Paginate
+            previousLabel={<IconLeft />}
+            nextLabel={<IconRight />}
+            breakLabel={"..."}
+            breakClassName={"break"}
+            pageCount={totalPages}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={4}
+            onPageChange={this.handlePageChange}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+          />
+        </PaginateWrapper>        
       </Container>
     );
   }
