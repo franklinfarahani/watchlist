84785c1d511d679dfb3d69ba70751c59487ef527
Franklin Farahani <dduction@gmail.com>
2018-10-28 11:05:49 -0700

    add test for formatDate

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,4 +1,4 @@
-import {getGenreName, getProvider, formatRuntime} from './';
+import {getGenreName, getProvider, formatRuntime, formatDate} from './';
 
 test('get "Drama" given genre id 6', () => {
   expect(getGenreName(6)).toBe('Drama');
@@ -10,4 +10,8 @@ test('get "Netflix" given provider id 8', () => {
 
 test('get 2 hours and 4 minutes given 124', () => {
   expect(formatRuntime(124)).toEqual({hours: 2, minutes: 4});
+});
+
+test('get 28 October 2018 given "2018-10-28"', () => {
+  expect(formatDate('2018-10-28')).toEqual({year: '2018', month: 'October', day: '28'});
 });
