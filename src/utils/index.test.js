54aa01b98d941c0c02a7e2aaf727a82dbd6a85a1
Franklin Farahani <dduction@gmail.com>
2018-10-28 10:59:15 -0700

    add test for formatRuntime

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,4 +1,4 @@
-import {getGenreName, getProvider} from './';
+import {getGenreName, getProvider, formatRuntime} from './';
 
 test('get "Drama" given genre id 6', () => {
   expect(getGenreName(6)).toBe('Drama');
@@ -6,4 +6,8 @@ test('get "Drama" given genre id 6', () => {
 
 test('get "Netflix" given provider id 8', () => {
   expect(getProvider(8).label).toBe('Netflix');
+});
+
+test('get 2 hours and 4 minutes given 124', () => {
+  expect(formatRuntime(124)).toEqual({hours: 2, minutes: 4});
 });
