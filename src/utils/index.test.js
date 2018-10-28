86bf62329a85ea75006146886cd2e7e3a432b059
Franklin Farahani <dduction@gmail.com>
2018-10-28 10:55:02 -0700

    add unit test for getProvider and fix test for getGenreName

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,5 +1,9 @@
-import {getGenreName} from './';
+import {getGenreName, getProvider} from './';
 
 test('get "Drama" given genre id 6', () => {
-  expect(getGenreName(6));
+  expect(getGenreName(6)).toBe('Drama');
+});
+
+test('get "Netflix" given provider id 8', () => {
+  expect(getProvider(8).label).toBe('Netflix');
 });
